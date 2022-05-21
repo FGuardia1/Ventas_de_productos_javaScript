b93ebf5924703daa1e74d538963ee3d545f926e9
Fernando Guardia <fguardia321@gmail.com>
2022-05-21 20:55:42 -0300

    Se aladio DOM y eventos

--- js/venta.js
+++ js/venta.js
@@ -45,11 +45,12 @@ function Venta(fecha, listaCompra, tarjeta) {
 }
 
 class Producto {
-  constructor(id, nombre, precio, stock) {
+  constructor(id, nombre, precio, stock, imagenPath) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
+    this.imagenPath = imagenPath;
   }
   descontarStock(cantidad) {
     this.stock = this.stock - cantidad;
@@ -73,49 +74,31 @@ class ProductoVta {
   }
 }
 
-//////////////Creo los productos
-const producto1 = new Producto(
-  0,
-  "Mouse Redragon Griffin M607 RGB USB 7200DPI",
-  1730,
-  20
-);
-const producto2 = new Producto(
-  1,
-  "Memoria Patriot Viper Steel 8gb Ddr4 3200mhz",
-  4720,
-  10
-);
-const producto3 = new Producto(
-  2,
-  "Placa de Video Powercolor Radeon RX 550 Red Dragon 4GB GDDR5",
-  35500,
-  5
-);
+function visualizarProd(arrayProd) {
+  let muestrarioProd = document.getElementById("listaProductos");
+  for (const producto of arrayProd) {
+    let contenedor = document.createElement("div");
 
-////Agrego los productos al array de productos
-arrayProd.push(producto1, producto2, producto3);
-
-////Hago un string del listado de codigo y nombre de los productos para luego mostrarlo por pantalla con prompt
-let listaProductos = "";
-for (const producto of arrayProd) {
-  listaProductos = listaProductos + "\n" + producto.id + "-" + producto.nombre;
+    contenedor.innerHTML = `<div class="articulo__nombre">
+    ${producto.nombre}
+  </div>
+  <div class="articulo__precio">$${producto.precio}</div>
+  <img class="articulo__imagen" src="${producto.imagenPath}" alt="" />
+  <button class="articulo__boton-compra" id=boton${producto.id}>COMPRAR</button>`;
+    contenedor.className = "articulo";
+    ////añado el producto que se va a ver en la pagina
+    muestrarioProd.appendChild(contenedor);
+    ///creo el evento de click para el boton de cada producto que se visualizara, le envio el id del producto a la funcion
+    let boton = document.getElementById("boton" + producto.id);
+    boton.onclick = () => {
+      enlistarProducto(producto.id);
+    };
+  }
 }
 
-/////////Aqui empieza el ingreso de datos de los productos a comprar
-let productoVta;
-let codProducto = parseInt(
-  prompt(
-    "Ingrese codigo de producto: " +
-      listaProductos +
-      "\n Ingrese -1 para terminar de ingresar productos "
-  )
-);
-
-///sigue preguntando para comprar productos hasta ingresar -1 como codigo de producto
-while (codProducto != -1) {
-  cantidad = parseInt(prompt("Ingrese cantidad que quiere comprar"));
-  ////compruebo que los datos ingresados esten en el rango de datos permitidos
+function enlistarProducto(codProducto) {
+  let productoVta;
+  let cantidad = prompt("Ingrese cantidad");
   if (!validarDatos(codProducto, cantidad)) {
     console.log("Datos ingresados no validos");
   } else {
@@ -124,7 +107,7 @@ while (codProducto != -1) {
 
     ///verifico que el stock actual del producto alcance a cubrir la cantidad pedida
     if (!productoVta.verificarStock(cantidad)) {
-      console.log("No hay suficiente stock");
+      alert("No hay suficiente stock");
     } else {
       ////Añado el producto a la lista de productos a comprar
       arrayProdVta.push(
@@ -135,54 +118,88 @@ while (codProducto != -1) {
           cantidad
         )
       );
-      console.log("Producto añadido a la lista");
+      alert("Producto añadido a la lista");
     }
   }
-
-  codProducto = parseInt(
-    prompt(
-      "Ingrese codigo de producto: " +
-        listaProductos +
-        "\n Ingrese -1 para terminar de ingresar productos "
-    )
-  );
 }
 
-if (arrayProdVta.length != 0) {
-  let nrotarjeta = prompt("Ingrese nro de tarjeta");
-  let titular = prompt("Ingrese titular de la tarjeta");
-  let tarjetaCVV = prompt("Ingrese CVV de la tarjeta");
-  const tarjeta1 = new Tarjeta(nrotarjeta, titular);
+function comprar() {
+  if (arrayProdVta.length != 0) {
+    let nrotarjeta = prompt("Ingrese nro de tarjeta");
+    let titular = prompt("Ingrese titular de la tarjeta");
+    let tarjetaCVV = prompt("Ingrese CVV de la tarjeta");
+    const tarjeta1 = new Tarjeta(nrotarjeta, titular);
 
-  ////realiza el pago por tarjeta, duvuelve true si pudo, sino false
-  if (!tarjeta1.realizarPago(tarjetaCVV)) {
-    console.log("No se pudo realizar el pago");
-  } else {
-    let fecha = new Date();
-    const venta1 = new Venta(fecha, arrayProdVta, tarjeta1.nro);
-    ///una vez registrada la compra hago el descuento de stock al/los producto/s correspondiente/s
-    //  y hago la lista de los productos para luego mostrarlo por pantalla
-    let listaCompra = "";
+    ////realiza el pago por tarjeta, duvuelve true si pudo, sino false
+    if (!tarjeta1.realizarPago(tarjetaCVV)) {
+      alert("No se pudo realizar el pago");
+    } else {
+      let fecha = new Date();
+      const venta1 = new Venta(fecha, arrayProdVta, tarjeta1.nro);
+      ///una vez registrada la compra hago el descuento de stock al/los producto/s correspondiente/s
+      //  y hago la lista de los productos para luego mostrarlo por pantalla
+      let listaCompra = "";
 
-    for (const producto of arrayProdVta) {
-      arrayProd[producto.id].descontarStock(cantidad);
-      listaCompra =
-        listaCompra +
-        "\n" +
-        producto.id +
-        "-" +
-        producto.nombre +
-        " cantidad:" +
-        producto.cantidad;
-    }
+      for (const producto of arrayProdVta) {
+        arrayProd[producto.id].descontarStock(producto.cantidad);
+        listaCompra =
+          listaCompra +
+          "\n" +
+          producto.id +
+          "-" +
+          producto.nombre +
+          " cantidad:" +
+          producto.cantidad;
+      }
 
-    /////informo por consola que la venta se realizo exitosamente
-    console.log(
-      "Se compro con exito:" +
+      /////informo que la venta se realizo exitosamente, reemplazando el texto/html que hay en el
+      /// elemento con ID=listaCompra, que tiene Aviso que no se compro nada aun por defecto
+      let anuncioCompra = document.getElementById("listaCompra");
+      let parrafo = document.createElement("p");
+      parrafo.innerText =
+        "Se compro con exito:" +
         listaCompra +
         "\n  por un precio total de $" +
         venta1.total +
-        "\n Puede pasar a retirar su compra por cualquiera de nuestras sucursales "
-    );
+        "\n Puede pasar a retirar su compra por cualquiera de nuestras sucursales ";
+      anuncioCompra.innerHTML = parrafo.innerHTML;
+
+      ////vacio el array de compra
+      while (arrayProdVta.length > 0) arrayProdVta.pop();
+    }
   }
 }
+
+//////////////Creo los productos
+const producto1 = new Producto(
+  0,
+  "Mouse Redragon Griffin M607 RGB USB 7200DPI",
+  1730,
+  20,
+  "./images/Mouse.png"
+);
+const producto2 = new Producto(
+  1,
+  "Memoria Patriot Viper Steel 8gb Ddr4 3200mhz",
+  4720,
+  10,
+  "./images/memoriaRam.png"
+);
+const producto3 = new Producto(
+  2,
+  "Placa de Video Powercolor Radeon RX 550 Red Dragon 4GB GDDR5",
+  35500,
+  5,
+  "./images/placaVideo.png"
+);
+
+////Agrego los productos al array de productos
+arrayProd.push(producto1, producto2, producto3);
+////visualizo todos los productos que estan en el array en la pagina web
+visualizarProd(arrayProd);
+
+///creo el evento de click para el boton de compra
+let botonCompra = document.getElementById("botonCompra");
+botonCompra.onclick = () => {
+  comprar();
+};
